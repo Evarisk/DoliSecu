3081f825c25e05e2aa7adf4abe2811b47336575d
Nicolas Domenech <nicolas@eoxia.com>
2022-11-18 19:40:40 +0100

    [Index] fix: error for chmod use 4 letter insted of 3

--- dolisecuindex.php
+++ dolisecuindex.php
@@ -24,32 +24,32 @@
 // Load Dolibarr environment
 $res = 0;
 // Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
-if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) {
-	$res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php";
+if (!$res && !empty($_SERVER['CONTEXT_DOCUMENT_ROOT'])) {
+	$res = @include $_SERVER['CONTEXT_DOCUMENT_ROOT']. '/main.inc.php';
 }
 // Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
 $tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME']; $tmp2 = realpath(__FILE__); $i = strlen($tmp) - 1; $j = strlen($tmp2) - 1;
 while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
 	$i--; $j--;
 }
-if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1))."/main.inc.php")) {
-	$res = @include substr($tmp, 0, ($i + 1))."/main.inc.php";
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1)). '/main.inc.php')) {
+	$res = @include substr($tmp, 0, ($i + 1)). '/main.inc.php';
 }
-if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php")) {
-	$res = @include dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php";
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1))). '/main.inc.php')) {
+	$res = @include dirname(substr($tmp, 0, ($i + 1))). '/main.inc.php';
 }
 // Try main.inc.php using relative path
-if (!$res && file_exists("../main.inc.php")) {
-	$res = @include "../main.inc.php";
+if (!$res && file_exists('../main.inc.php')) {
+	$res = @include '../main.inc.php';
 }
-if (!$res && file_exists("../../main.inc.php")) {
-	$res = @include "../../main.inc.php";
+if (!$res && file_exists('../../main.inc.php')) {
+	$res = @include '../../main.inc.php';
 }
-if (!$res && file_exists("../../../main.inc.php")) {
-	$res = @include "../../../main.inc.php";
+if (!$res && file_exists('../../../main.inc.php')) {
+	$res = @include '../../../main.inc.php';
 }
 if (!$res) {
-	die("Include of main fails");
+	die('Include of main fails');
 }
 
 require_once DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php';
@@ -63,8 +63,8 @@ require_once DOL_DOCUMENT_ROOT.'/core/class/events.class.php';
 
 
 // Load translation files required by the page
-$langs->loadLangs(array("dolisecu@dolisecu"));
-$langs->loadLangs(array("admin", "errors"));
+$langs->loadLangs(array('dolisecu@dolisecu'));
+$langs->loadLangs(array('admin', 'errors'));
 
 $action = GETPOST('action', 'aZ09');
 
@@ -105,14 +105,14 @@ if (($perms & 0x0004) || ($perms & 0x0002) || !file_exists($installlock)) {
 
 if ($action == 'check') {
 	if (($perms & 0x0004) || ($perms & 0x0002)) {
-		chmod($dolibarr_main_document_root.'/'.$conffile, 440);
+		$test = chmod($dolibarr_main_document_root.'/'.$conffile, 0444);
 		setEventMessage($langs->trans('ConfFileSetPermissions'));
 	}
 	if (!file_exists($installlock)) {
-		fopen(DOL_DATA_ROOT. "/install.lock", "w");
+		fopen(DOL_DATA_ROOT. '/install.lock', 'w');
 		setEventMessage($langs->trans('InstallLockFileCreated'));
 	}
-	header("Location: " . $_SERVER['PHP_SELF']);
+	header('Location: ' . $_SERVER['PHP_SELF']);
 }
 
 /*
@@ -122,42 +122,42 @@ if ($action == 'check') {
 $form = new Form($db);
 $formfile = new FormFile($db);
 
-llxHeader("", $langs->trans("ModuleDoliSecuName"));
+llxHeader('', $langs->trans('ModuleDoliSecuName'));
 
-print load_fiche_titre($langs->trans("ModuleDoliSecuName"), '', 'dolisecu.png@dolisecu');
+print load_fiche_titre($langs->trans('ModuleDoliSecuName'), '', 'dolisecu.png@dolisecu');
 
-print '<a class="' . ($need_repair == 1 ? 'butAction' : 'butActionRefused classfortooltip') . '" id="actionButtonCheck" title="' . ($need_repair == 1 ? '' : dol_escape_htmltag($langs->trans("NoSecurityProblem"))) . '" href="' . ($need_repair == 1 ? ($_SERVER["PHP_SELF"] . '?id=' . $object->id . '&action=check') : '#') . '">' . $langs->trans("Repair") . ' ' . $langs->trans("SecurityProblem") . '</a>';
+print '<a class="' . ($need_repair == 1 ? 'butAction' : 'butActionRefused classfortooltip') . '" id="actionButtonCheck" title="' . ($need_repair == 1 ? '' : dol_escape_htmltag($langs->trans('NoSecurityProblem'))) . '" href="' . ($need_repair == 1 ? ($_SERVER['PHP_SELF'] . '?id=' . $object->id . '&action=check') : '#') . '">' . $langs->trans('Repair') . ' ' . $langs->trans('SecurityProblem') . '</a>';
 
 print '<br>';
 print '<br>';
 
 // $conffile is defined into filefunc.inc.php
-print '<strong>'.$langs->trans("PermissionsOnFile", $conffile).'</strong>: ';
+print '<strong>'.$langs->trans('PermissionsOnFile', $conffile).'</strong>: ';
 if ($perms) {
 	if (($perms & 0x0004) || ($perms & 0x0002)) {
-		print img_warning().' '.$langs->trans("ConfFileIsReadableOrWritableByAnyUsers");
+		print img_warning().' '.$langs->trans('ConfFileIsReadableOrWritableByAnyUsers');
 		// Web user group by default
 		$labeluser = dol_getwebuser('user');
 		$labelgroup = dol_getwebuser('group');
-		print ' '.$langs->trans("User").': '.$labeluser.':'.$labelgroup;
+		print ' '.$langs->trans('User').': '.$labeluser.':'.$labelgroup;
 		if (function_exists('posix_geteuid') && function_exists('posix_getpwuid')) {
 			$arrayofinfoofuser = posix_getpwuid(posix_geteuid());
 			print ' <span class="opacitymedium">(POSIX '.$arrayofinfoofuser['name'].':'.$arrayofinfoofuser['gecos'].':'.$arrayofinfoofuser['dir'].':'.$arrayofinfoofuser['shell'].')</span>';
 		}
 	} else {
-		print img_picto('', 'tick').' '.$langs->trans("ConfFileHasGoodPermissions");
+		print img_picto('', 'tick').' '.$langs->trans('ConfFileHasGoodPermissions');
 	}
 } else {
-	print img_warning().' '.$langs->trans("FailedToReadFile", $conffile);
+	print img_warning().' '.$langs->trans('FailedToReadFile', $conffile);
 }
 print '<br>';
 print '<br>';
 
-print '<strong>'.$langs->trans("DolibarrSetup").'</strong>: ';
+print '<strong>'.$langs->trans('DolibarrSetup').'</strong>: ';
 if (file_exists($installlock)) {
-	print img_picto('', 'tick').' '.$langs->trans("InstallAndUpgradeLockedBy", $installlock);
+	print img_picto('', 'tick').' '.$langs->trans('InstallAndUpgradeLockedBy', $installlock);
 } else {
-	print img_warning().' '.$langs->trans("WarningLockFileDoesNotExists", DOL_DATA_ROOT);
+	print img_warning().' '.$langs->trans('WarningLockFileDoesNotExists', DOL_DATA_ROOT);
 }
 print '<br>';
 
